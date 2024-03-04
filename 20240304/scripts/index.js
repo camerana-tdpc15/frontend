105b129b2a1019a4ceb6141aae9111e29149a3cf
Mr D <danilo.tdpc@gmail.com>
2024-03-04 21:21:20 +0100

    Add files via upload

---
+++ 20240304/scripts/index.js
@@ -0,0 +1,33 @@
+console.log('inizio file...');
+
+// questo e' un commento su una linea
+
+/*
+    questo e' un
+    commento
+    multilinea 
+*/
+
+const firstName = 'Walter';
+var lastName = "White";
+let age = 60;
+var average = 5.5;
+var isAlive = false;
+
+// valori speciali
+var x = null;
+var y = undefined;
+
+firstName = 42.56789;
+
+
+
+if (age >= 18) { 
+    console.log('maggiorenne'); 
+}
+else {
+    console.log('minorenne');
+}
+
+
+console.log('qui lo script finisce');
